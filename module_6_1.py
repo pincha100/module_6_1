3978ae0d3dd51bde501ee6be49ad88258c628730
pincha100 <113956174+pincha100@users.noreply.github.com>
2024-09-10 00:21:23 +0700

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,40 +1,37 @@
 class Animal:
+    alive = True
+    fed = False
+
     def __init__(self, name):
         self.name = name
-        self.alive = True
-        self.fed = False
+
+    def eat(self, food: 'Plant'):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 
 class Plant:
+    edible = False
+
     def __init__(self, name):
         self.name = name
-        self.edible = False
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            print(f"{self.name} съел {food.name}")
-        else:
-            self.alive = False
-            print(f"{self.name} не стал есть {food.name}")
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            print(f"{self.name} съел {food.name}")
-        else:
-            self.alive = False
-            print(f"{self.name} не стал есть {food.name} и погиб")
+    pass
 
 class Flower(Plant):
     pass
 
 class Fruit(Plant):
-    def __init__(self, name):
-        super().__init__(name)
-        self.edible = True
+    edible = True
 
+# Проверка работы
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
@@ -49,4 +46,3 @@ a1.eat(p1)
 a2.eat(p2)
 print(a1.alive)
 print(a2.fed)
-
